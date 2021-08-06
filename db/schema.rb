c492d9ef7ffc3611adf20d5d09095ec132281489
KiraElric <lissette.urso@gmail.com>
2021-08-06 00:21:16 -0400

    Add index to bookcollections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_033304) do
+ActiveRecord::Schema.define(version: 2021_08_06_041457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_08_06_033304) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id", "user_id"], name: "index_book_collections_on_book_id_and_user_id", unique: true
     t.index ["book_id"], name: "index_book_collections_on_book_id"
     t.index ["user_id"], name: "index_book_collections_on_user_id"
   end
