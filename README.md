72c17b828e4541e11b597bbab3a68e2a56ec6f07
KiraElric <lissette.urso@gmail.com>
2021-08-06 22:02:59 -0400

    readme y seed

--- README.md
+++ README.md
@@ -1,24 +1,12 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+Una vez descargado el proyecto:
+1. Ejecutar el comando bundle install para instalar las gemas utilizadas en el proyecto.
+2. Ejecutar "npm/yarn install" para instalar las dependencias de frontend.
+3. Levantar el motor de base de datos PostgreSQL.
+4. Configurar las credenciales para conectar el proyecto con el motor de base de datos en el archivo config/database.yml.
+5. Ejecutar el comando "rails db:create" para crear la base de datos en el motor de PostgreSQL.
+6. Ejecutar el comando "rails db:migrate" para realizar las migraciones de los modelos del proyecto.
+7. Ejecutar el comando "rails db:seed" para poblar la base de datos del proyecto y contar con datos con los cuales trabajar.
+8. Ejecutar "rails s" para levantar el proyecto e ingresar a la ruta "localhost:3000"
+ 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+users = [[email: 'lissette.urso@example.com', password: '123456', name: 'Lissette', lastname: 'Urso', phone: '951796426', admin: true],
+          [email: 'pepa@example.com', password: '123456', name: 'Pepa', lastname: 'Urso', phone: '965235443', admin: false],
+          [email: 'nova@example.com', password: '123456', name: 'Nova', lastname: 'Vivallos', phone: '954673456', admin: false]]
+
+puts "Creando Usuarios"
+users.each do |user|
+  current_user = User.create(email: user[:email], 
+                            password: user[:password],
+                            name: user[:name],
+                            lastname: user[:lastname],
+                            phone: user[:phone],
+                            admin: user[:admin])
+  
+  puts "Se ha creado el usuario #{current_user.name} #{current_user.lastname}"
+end
+
 puts "Creando autores"
 50.times do |i|
   authors_name = Faker::Book.author.split(' ')
@@ -34,3 +50,11 @@ puts "Creando Libros"
                     publisher: Publisher.find(rand(1..50)))
   puts "Se ha creado el libro #{book.title}"
 end
+
+# puts "Creando Book Collections"
+# 50.times do |i|
+#   book_collection = BookCollection.create(user_id:,
+#                                           book_id:,
+#                                           state:,
+#                                           owned:)
+# end
